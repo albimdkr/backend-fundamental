2716d32818e9245521483db8c8ab5f892af7c3fb
albimdkr <albimudakar04@gmail.com>
2023-05-25 00:10:26 +0700

    add projectnodejs

---
+++ projectNodejs/index.js
@@ -0,0 +1,8 @@
+console.log("Menyalakan mesin");
+console.log("Grinder beans");
+console.log("Memanaskan air");
+console.log("Siapkan Tisu");
+console.log("Siapkan Driver V60");
+console.log("Tuangkan Coffee");
+console.log("Brewing coffee");
+console.log("Coffee siap dinikmati");
