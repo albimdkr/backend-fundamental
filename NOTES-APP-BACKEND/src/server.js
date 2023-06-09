69136b17746a5d8cc01aa968c50e2af6a70689c5
albimdkr <albimudakar04@gmail.com>
2023-06-10 00:42:09 +0700

    add method POST

--- NOTES-APP-BACKEND/src/server.js
+++ NOTES-APP-BACKEND/src/server.js
@@ -1,11 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server its run on ${server.info.uri}`);
