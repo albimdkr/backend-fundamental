88b0d81aa237b89333f87a580bbd4d29cf22d2de
albimdkr <albimudakar04@gmail.com>
2023-06-09 23:21:18 +0700

    add server.js

--- NOTES-APP-BACKEND/src/server.js
+++ NOTES-APP-BACKEND/src/server.js
@@ -1,2 +1,14 @@
-// eslint-disable-next-line no-console
-console.log('Server Merespon!');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server its run on ${server.info.uri}`);
+};
+
+init();
